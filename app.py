20253bfa0847dd8611cf3382e76fec985a505442
kuba <winczukjakub@gmail.com>
2022-12-21 23:56:46 +0100

    added CORS to accept requests from android ionic app

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 
 from solver import Solver
 from imageprocessing import processing
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
